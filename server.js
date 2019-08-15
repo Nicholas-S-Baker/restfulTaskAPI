c83eebb8f97f57c919613714c66649e98d615789
Nicholas Baker <n.spen.baker@gmail.com>
2019-08-15 13:11:10 -0600

    added angular

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 
 app.use(express.json());
+app.use(express.static( __dirname + '/angular-app/dist/angular-app' ));
 
 mongoose.connect("mongodb://localhost/restful_task", {
   useNewUrlParser: true,
